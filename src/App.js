b2a1111d97496189d93752fde2bb7ffac044d9d0
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-01 09:19:21 +0900

    Fix : 가독성을 위해 줄 띄움 처리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import { styled } from "styled-components";
 import CenteredComponent from "./components/CenteredComponent";
 import { GridStyle } from "./style/gridStyle";
-import { styled } from "styled-components";
-
-import "./style/App.css";
 import FirstSquare from "./components/FirstSquare";
-
 import SecondSquare from "./components/SecondSquare";
 import ThirdSquare from "./components/ThirdSquare";
 import FourthSquare from "./components/FourthSquare";
+
+import "./style/App.css";
+
 const App = () => {
   return (
     <>
